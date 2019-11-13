bd932759bd86bef7b8fad56a931bdd216ca56230
Vadim Korostelev <vdmkbu@ya.ru>
2019-11-13 21:45:24 +0500

    роли пользователя

--- app/User.php
+++ app/User.php
@@ -10,6 +10,9 @@ class User extends Authenticatable
 {
     use Notifiable;
 
+    public const ROLE_USER = 'user';
+    public const ROLE_ADMIN = 'admin';
+
     /**
      * The attributes that are mass assignable.
      *
