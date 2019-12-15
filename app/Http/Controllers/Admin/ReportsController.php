58ab7c16e9123ab54f3073068d37219b11f30a1e
Vadim Korostelev <vdmkbu@ya.ru>
2019-12-15 13:29:28 +0500

    отчёт по должностям

--- app/Http/Controllers/Admin/ReportsController.php
+++ app/Http/Controllers/Admin/ReportsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Log;
+use App\Position;
 use App\Project;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
@@ -74,13 +75,74 @@ class ReportsController extends Controller
 
         $logs = $query->get();
 
-        $projects = Project::active()->orderBy('name','asc')->get();
+        $projects = Project::orderBy('name','asc')->get();
 
         return view('admin.reports.projects', compact('logs', 'projects', 'interval'));
     }
 
-    public function positions()
+    public function positions(Request $request)
     {
-        return view('admin.reports.positions');
+
+        $query = DB::table('logs')
+            ->select('positions.name as position')
+            ->selectRaw(DB::raw('SUM(time) as time'))
+            ->join('users', 'logs.user_id','=', 'users.id')
+            ->join('positions','users.position_id','=','positions.id')
+            ->groupBy('users.position_id');
+
+
+        // если нет параметров, то выводим за текущий месяц
+        if (empty($request->get('from_date')) && empty($request->get('to_date'))) {
+            $query->where('date', 'LIKE', Carbon::now()->format('Y-m') . '-%');
+
+
+            $interval = [
+                'start' => (new Carbon('first day of this month'))->format('Y.m.d'),
+                'end' => (new Carbon('last day of this month'))->format('Y.m.d')
+            ];
+
+
+        }
+
+        // если выбран интервал
+        if (!empty($from_date = $request->get('from_date')) && !empty($to_date = $request->get('to_date'))) {
+            $query->where('date', '>=', $from_date)->where('date', '<=', $to_date);
+
+            $interval = [
+                'start' => (new Carbon($from_date))->format('Y.m.d'),
+                'end' => (new Carbon($to_date))->format('Y.m.d')
+            ];
+        }
+
+        if(!empty($from_date = $request->get('from_date')) && empty($request->get('to_date'))) {
+            $query->where('date', '>=', $from_date);
+
+            $interval = [
+                'start' => (new Carbon($from_date))->format('Y.m.d'),
+                'end' => (new Carbon())->format('Y.m.d')
+            ];
+        }
+
+        if(empty($request->get('from_date')) && !empty($to_date = $request->get('to_date'))) {
+            $query->where('date', '<=', $to_date);
+
+            $interval = [
+                'start' => '',
+                'end' => (new Carbon($to_date))->format('Y.m.d')
+            ];
+        }
+
+        if (!empty($value = $request->get('position'))) {
+
+            $query->where('users.position_id', $value);
+        }
+
+        $logs = $query->get();
+
+        $positions = Position::orderBy('name', 'asc')->get();
+
+        return view('admin.reports.positions', compact('logs', 'positions', 'interval'));
     }
+
+
 }
--- resources/views/admin/reports/positions.blade.php
+++ resources/views/admin/reports/positions.blade.php
@@ -12,4 +12,79 @@
         </li>
 
     </ul>
+
+    <div class="card mb-3">
+        <div class="card-header">Filter</div>
+        <div class="card-body">
+            <form action="?" method="GET">
+                <div class="row">
+                    <div class="col-sm-2">
+                        <div class="form-group">
+                            <label for="from_date" class="col-form-label">From date</label>
+                            <input id="from_date" class="form-control"
+                                   name="from_date"
+                                   value="{{ request('from_date') }}">
+                        </div>
+                    </div>
+                    <div class="col-sm-2">
+                        <div class="form-group">
+                            <label for="to_date" class="col-form-label">To date</label>
+                            <input id="to_date" class="form-control"
+                                   name="to_date"
+                                   value="{{ request('to_date') }}">
+                        </div>
+                    </div>
+                    <div class="col-sm-2">
+                        <div class="form-group">
+                            <label for="position" class="col-form-label">Position</label>
+                            <select id="position" class="form-control" name="position">
+                                <option value=""></option>
+                                @foreach ($positions as $position)
+                                    <option value="{{ $position->id }}"{{ $position->id == request('position') ? ' selected' : '' }}>
+                                        {{ $position->name }}
+                                    </option>
+                                @endforeach;
+                            </select>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-1">
+                        <div class="form-group">
+                            <label class="col-form-label">&nbsp;</label><br />
+                            <button type="submit" class="btn btn-primary">Search</button>
+                        </div>
+
+                    </div>
+                    <div class="col-sm-1">
+                        <div class="form-group">
+                            <label class="col-form-label">&nbsp;</label><br />
+                            <button type="button" onclick="window.location = window.location.href.split('?')[0];" class="btn btn-primary">Reset</button>
+                        </div>
+
+                    </div>
+
+                </div>
+            </form>
+        </div>
+    </div>
+
+    <table class="table table-bordered table-striped">
+        <thead>
+        <tr>
+            <th>Interval</th>
+            <th>Position</th>
+            <th>Time total</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach ($logs as $log)
+            <tr>
+                <td>{{ $interval['start'] }} — {{ $interval['end'] }}</td>
+                <td>{{ $log->position }}</td>
+                <td>{{ $log->time }}</td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+
 @endsection
